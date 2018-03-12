49de3fbb40439f29a20e135df58edd6d550e36db
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-03-12 18:40:19 +0100

    Mejorar el código y hacer una solución decente (de hecho creo que es incapaz de perder, aunque no lo he demostrado)

--- puntuaciones-1.rs
+++ puntuaciones-1.rs
@@ -4,21 +4,57 @@ use std::ops::IndexMut;
 fn main() {
     use Celda::*;
     let tablero: Tablero =
-        [ Vacia, Vacia, Circulo
-        , Cruz, Cruz , Vacia
-        , Cruz , Vacia, Circulo
+        [ Vacia, Vacia, Vacia
+        , Vacia, Vacia, Vacia
+        , Vacia, Vacia, Vacia
         ].into();
-    //println!("{}", tablero.puntuacion(Circulo));
-    println!(
-        "{:?}",
-        tablero
-            .acciones()
-            .iter()
-            .map(|&x| (x, tablero.turno(x).puntuacion(Circulo)))
-            .max_by(|&(_, x), &(_, y)| x.cmp(&y))
-            .unwrap()
-            .0
-    );
+    println!("{:?}", siguiente_jugada(tablero, Jugador::Cruz));
+}
+
+fn mejor_puntuacion(tablero: &Tablero, jugador: Jugador) -> (Accion, i32) {
+    tablero
+        .acciones(jugador)
+        .iter()
+        .map(|&x| (x, tablero.efectuar_accion(x).puntuacion(jugador)))
+        .max_by(|&(_, x), &(_, y)| x.cmp(&y))
+        .unwrap()
+}
+
+fn siguiente_jugada(tablero: Tablero, jugador: Jugador) -> Accion {
+    let p1 = mejor_puntuacion(&tablero, jugador);
+    let p2 = mejor_puntuacion(&tablero, jugador.opuesto());
+    if p1.1 >= p2.1 {
+        p1.0
+    } else {
+        match p2.0 {
+            Accion::Cruz(i) => Accion::Circulo(i),
+            Accion::Circulo(i) => Accion::Cruz(i),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+enum Jugador {
+    Cruz,
+    Circulo,
+}
+
+impl Jugador {
+    fn opuesto(self) -> Jugador {
+        match self {
+            Jugador::Cruz => Jugador::Circulo,
+            Jugador::Circulo => Jugador::Cruz,
+        }
+    }
+}
+
+impl Into<Celda> for Jugador {
+    fn into(self) -> Celda {
+        match self {
+            Jugador::Cruz => Celda::Cruz,
+            Jugador::Circulo => Celda::Circulo,
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
@@ -78,16 +114,15 @@ static lineas: [[usize; 3]; 8] =
     ];
 
 impl Tablero {
-    fn puntuacion(&self, jugador: Celda) -> i32 {
-        use Celda::*;
-        assert!(jugador != Vacia);
+    fn puntuacion(&self, jugador: Jugador) -> i32 {
+        if self.es_fin_de_partida() { return 8*4 }
         let mut puntuacion = 0;
         for l in lineas.iter() {
             let mut p = 0;
             for &i in l.iter() {
-                if self[i] == jugador {
+                if self[i] == jugador.into() {
                     p += 1;
-                } else if self[i] != Vacia {
+                } else if self[i] != Celda::Vacia {
                     p = -1;
                     break;
                 }
@@ -95,6 +130,22 @@ impl Tablero {
             puntuacion += p + 1;
         }
         puntuacion
+    }
+
+    fn dar_la_vuelta(&self) -> Tablero {
+        let t =
+            self
+                .0
+                .iter()
+                .map(|&celda| {
+                    match celda {
+                        Celda::Cruz => Celda::Circulo,
+                        Celda::Circulo => Celda::Cruz,
+                        Celda::Vacia => Celda::Vacia,
+                    }
+                })
+                .collect::<Vec<Celda>>();
+        Tablero([t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7], t[8]])
     }
 
     fn es_fin_de_partida(&self) -> bool {
@@ -111,7 +162,7 @@ impl Tablero {
         || (self[2] != Vacia && self[2] == self[4] && self[4] == self[6])
     }
 
-    fn ganador(&self) -> Celda {
+    fn ganador(&self) -> Option<Jugador> {
         use Celda::*;
         if (self[0] == Cruz && self[0] == self[1] && self[1] == self[2])
         || (self[3] == Cruz && self[3] == self[4] && self[4] == self[5])
@@ -123,7 +174,7 @@ impl Tablero {
 
         || (self[0] == Cruz && self[0] == self[4] && self[4] == self[8])
         || (self[2] == Cruz && self[2] == self[4] && self[4] == self[6]) {
-            Cruz
+            Some(Jugador::Cruz)
         } else if
                (self[0] == Circulo && self[0] == self[1] && self[1] == self[2])
             || (self[3] == Circulo && self[3] == self[4] && self[4] == self[5])
@@ -135,13 +186,28 @@ impl Tablero {
 
             || (self[0] == Circulo && self[0] == self[4] && self[4] == self[8])
             || (self[2] == Circulo && self[2] == self[4] && self[4] == self[6]) {
-            Circulo
+            Some(Jugador::Circulo)
         } else {
-            Vacia
+            None
         }
     }
 
-    fn acciones(&self) -> Vec<Accion> {
+    fn acciones(&self, jugador: Jugador) -> Vec<Accion> {
+        let mut vec = Vec::new();
+        let mut i = 0;
+        for &x in self.0.iter() {
+            if x == Celda::Vacia {
+                vec.push(match jugador {
+                    Jugador::Cruz => Accion::Cruz(i),
+                    Jugador::Circulo => Accion::Circulo(i),
+                });
+            }
+            i += 1;
+        }
+        vec
+    }
+
+    fn turno(&self) -> Jugador {
         let mut i = 0;
         for &x in self.0.iter() {
             match x {
@@ -151,33 +217,14 @@ impl Tablero {
             }
         }
         match i {
-            0 | -1 => {  // mismo número de cruces y círculos o hay un círculo más
-                let mut vec = Vec::new();
-                let mut i = 0;
-                for &x in self.0.iter() {
-                    if x == Celda::Vacia {
-                        vec.push(Accion::Cruz(i));
-                    }
-                    i += 1;
-                }
-                vec
-            },
-            1 => {
-                let mut vec = Vec::new();
-                let mut i = 0;
-                for &x in self.0.iter() {
-                    if x == Celda::Vacia {
-                        vec.push(Accion::Circulo(i));
-                    }
-                    i += 1;
-                }
-                vec
-            },
+            // mismo número de cruces y círculos o hay un círculo más
+            0 | -1 => Jugador::Cruz,
+            1 => Jugador::Circulo,
             _ => panic!("¡Alguien ha hecho trampas!"),
         }
     }
 
-    fn turno(&self, accion: Accion) -> Tablero {
+    fn efectuar_accion(&self, accion: Accion) -> Tablero {
         let mut nuevo = self.clone();
         match accion {
             Accion::Cruz(i) => {
